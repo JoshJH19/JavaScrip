3797e06a824a22c7952d211a4de10a9f8a4dd2bd
Joshua <jimenesulises522@gmail.com>
2026-07-04 14:23:40 -0600

    Actualizacion

--- Tarea.js
+++ Tarea.js
@@ -1,12 +1,7 @@
-function multiplicar(num , max){
-        if(max === 0){
-            max = 10;
-        }else{
+function multiplicar(num , max = 10){
         console.log("====="+"Tabla de multiplicar: "+num+"=====");
         for (let i = 1;i <= max;i++){
             console.log(num + "X"+ i + "=" + (num*i));
         }
     }
-    }
-
 multiplicar(7);
